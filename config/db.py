4b3d968f0060afabc6d5665859c775765dd95939
franyober <franyober123@hotmail.com>
2024-12-27 15:43:18 -0500

    creando archivo de configuraciones

--- config/db.py
+++ config/db.py
@@ -1,5 +1,13 @@
 from pymongo import MongoClient
+import json
 
-conn = MongoClient("mongodb://192.168.1.14:27017")
+
+with open("config.json") as f:
+    config = json.load(f)
+
+server_url = config["server_url"]
+
+print(server_url)
+conn = MongoClient(f"mongodb://{server_url}:27017")
 
 
