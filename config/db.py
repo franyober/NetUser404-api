efe4d78295838838e6202257a74dd147fdb93b10
franyober <franyober123@hotmail.com>
2024-12-28 15:18:08 -0500

    cambio en la configuracion

--- config/db.py
+++ config/db.py
@@ -1,12 +1,9 @@
 from pymongo import MongoClient
 import json
 
-
 with open("config.json") as f:
     config = json.load(f)
-
 server_url = config["server_url"]
-
 print(server_url)
 conn = MongoClient(f"mongodb://{server_url}:27017")
 
