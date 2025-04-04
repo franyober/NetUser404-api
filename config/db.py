5b133cf5e14d980941c94a799d804f59248e57c2
franyober <franyober123@hotmail.com>
2025-04-04 15:13:22 -0500

    añadiendo un tiempo de espera para la selección del servidor en la conexión a MongoDB

--- config/db.py
+++ config/db.py
@@ -5,6 +5,6 @@ with open("config/config.json") as f:
     config = json.load(f)
 server_url = config["server_url"]
 print(server_url)
-conn = MongoClient(f"mongodb://{server_url}:27017")
+conn = MongoClient(f"mongodb://{server_url}:27017", serverSelectionTimeoutMS=4000)
 
 
